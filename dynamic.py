1ce881e574d8b7d49c5268acc92b91b869d55110
youssefhossam95 <youssefhossam95@hotmail.com>
2019-10-13 18:22:38 +0200

    added py files

---
+++ dynamic.py
@@ -0,0 +1,59 @@
+
+class MyModel(AutoTrainingModel):
+    """A class that represents any ML model registered in the framework.
+    """
+
+    
+    def constructModelSets(self,processedData:Iterable[pd.DataFrame])->ModelDataSetsHolder:
+        """Builds the model's training and test sets.
+        
+        Args:
+            processedData (Iterable[pd.DataFrame]): Processed data frames returned from DataManager.processData        
+        Returns:
+            ModelDataSetsHolder: The model's training and test sets wrapped in a ModelSetHolder object. 
+            You can refer to the "ModelSetsHolder" class in the "model.py" module for more details.
+        """
+        
+    
+    def trainAndEvalModel(self,modelSets:ModelDataSetsHolder,firstModel:object,latestModel:object)->ModelEvalOutput:
+        """Trains the model and compares its accuracy to the both the first model and the latest models, 
+        to detect any abnormal drop in accuracy that may be caused by some bug or data error.
+        
+        Args:
+            modelSets (ModelDataSetsHolder): Training and test sets.
+            firstModel (object): The initial model at the time of registering the package.
+            latestModel (object): The last model pushed to production.
+        Returns:
+            ModelEvalOutput: Object representing the model evaluation result.
+        """
+        
+        
+
+
+    def getInitialModelRelativePath(self): #relative path from the code file implementing this method.
+        """ 
+        Returns:
+        str: the relative path of the initial model that was built and trained during the prototyping phase.
+        The path should be relative to the the directory containing this file. 
+        """
+        
+
+
+    def loadModelFromPath(self,path:str):
+        """ Returns the model object representing the model file in the path provided.
+        Args:
+            path (str): full path to the model file (pkl,h5,etc.).
+        Returns:
+        Object: model object (keras model object, sklearn, pytorch, etc.) 
+        """
+        
+    
+
+    
+    def saveModelToPath(self,modelObj:object,path:str):
+        """ Saves the passed model object to a model file (h5,pkl,etc.) in the path provided.
+        Args:
+            modelObj (object): model object (keras model object, sklearn, pytorch, etc.)
+            path (str): full path to the model file.
+        """
+        pass  
---
+++ static.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+from autotraining.model import *
+import pandas as pd
+from typing import Iterable,Dict,Union,List,Tuple
+from enum import Enum
+
+
+"""    
+NOTE: all methods marked with the comment '#TODO: OPTIONAL METHOD' just before their definition are optional.
+An optional function is one that is already implemented in the base class so if you didn't implement it, it the base class method will be used instead.
+The base class method behaviour is described in each method's summary.
+"""
+
+
+
+class MyModelsEvaluator(ModelsEvaluator):
+    
+    
+    #TODO: OPTIONAL METHOD 
+    def evalModels(self,modelsResults:Dict[str,ModelEvalOutput])->ModelsEvaluationResult:
+        """
+        Evaluates all models based on the output of each model's trainEvalModel function. Default  behaviour is to only save models that returned true from its trainEval function.
+        Args:   
+            modelsResults (Dict[str,ModelEvalOutput]): dictionary with pairs {modelClassName:object representing the model evaluation output.}
+        Returns:
+            ModelsEvaluationResult: Returns the result of the models evaluation using Enum class ModelsEvaluationResult. 
+            The available options are: Save all new models, Save models with true evaluation only, ignore all models.
+            Check the enum class ModelsEvaluationResult in the model.py module for more details
+            to .
+        """
+        return ModelsEvaluationResult.SAVE_TRUE_ONLY
+
+
+
+class MyDataManager(DataManager):
+    """ A class for data related methods such as extraction,preprocessing and sampling. A subclass of this class that overrides all the abstract methods must be included in any package added to the framework.
+    """
+    
+
+    #TODO: OPTIONAL METHOD
+    def sampleRawData(self,rawData:Iterable[pd.DataFrame])->Iterable[pd.DataFrame]:
+        """Samples the raw data, so that the mock run carried out during the installation of the package is executed on a small subset of the data.
+        
+        Args:
+            rawData (Iterable[pd.DataFrame]): Data frames containing raw data.
+        
+        Returns:
+            Iterable[pd.DataFrame]: Data frames containing a subset of raw data that will be used in the mock run.
+        """
+        
+    
+    #TODO: OPTIONAL METHOD
+    def describeRawData(self,rawData:Iterable[pd.DataFrame])->Dict[str,Union[Iterable,float]]:
+        """Carries out some simple statistics on the new raw data such as the size of the dataframes, 
+        percentage of certain basins, number of wells, etc. If not implemented ,the default behaviour in the base class is to return the shapes of 
+        all data frames in raw data.
+        
+        Args:
+            rawData (Iterable[pd.DataFrame]): Iterable of data frames that represents raw data.
+        
+        Returns:
+            Dict[str,Union[Iterable,float]]: Dictionary containing pairs of ("Measure/Metric Name":Val)
+        """
+
+       
+
+    def extractRawData(self)->Iterable[pd.DataFrame]:
+        """Extracts the raw data from any data source such as a database, csv files, etc. 
+        Returns:
+            Iterable[pd.DataFrame]: Data frames representing the raw data.
+        """
+        
+
+
+    
+    def processData(self,rawData:Iterable[pd.DataFrame])->Iterable[pd.DataFrame]:
+        """Carries out any necassary pre-processing on the raw data, 
+        such as removing data errors, unit conversion, aliasing, arps fitting, peak shifting etc.
+        
+        Args:
+            rawData (Iterable[pd.DataFrame]): Data frames representing the new raw data that were returned from the extractRawData method.
+        
+        Returns:
+            Iterable[pd.DataFrame]: Processed data frames.
+        """
+        
+
+
